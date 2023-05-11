7661b7f88e37467c02817db17d9e4e3618e28e3c
Ricardo Kreyhsig <rickreyhsig@gmail.com>
2023-05-11 02:57:36 +0000

    Add sessions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,12 @@
+class SessionsController < ApplicationController
+  def create
+    @user = User.find_or_create_by(full_name: auth_hash.info.name, member_id: auth_hash.info.user_id)
+    session[:user_id] = @user.id
+    redirect_to '/'
+  end
+
+  protected
+  def auth_hash
+    request.env['omniauth.auth']
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/create'
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/auth/slack/callback', to: 'sessions#create'
 end
